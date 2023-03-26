2f018688601c63911ee0d443fef9ce8fb5b82a5e
DixLan <93582478+DixLan@users.noreply.github.com>
2023-03-26 14:42:32 +0200

    Add files via upload

---
+++ CacheFiveM.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+rmdir "C:\Users\%username%\AppData\Local\FiveM\FiveM.app\crashes" /s /q
+rmdir "C:\Users\%username%\AppData\Local\FiveM\FiveM.app\logs" /s /q
+rmdir "C:\Users\%username%\AppData\Local\FiveM\FiveM.app\data\cache" /s /q
+rmdir "C:\Users\%username%\AppData\Local\FiveM\FiveM.app\data\nui-storage" /s /q
+rmdir "C:\Users\%username%\AppData\Local\FiveM\FiveM.app\data\server-cache" /s /q
+rmdir "C:\Users\%username%\AppData\Local\FiveM\FiveM.app\data\server-cache-priv" /s /q
